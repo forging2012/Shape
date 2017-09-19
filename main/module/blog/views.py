c0c7c89942ae51020008bd08c2b1b42cdf09316a
root <root@localhost.localdomain>
2017-09-19 08:35:07 +0800

    v1.04 add User

--- main/module/blog/views.py
+++ main/module/blog/views.py
@@ -5,13 +5,32 @@ from flask import render_template
 from main.forms import CommentForm
 from main.models import Post
 
+"""
+主要展示好评、推荐文章
+"""
 @blog_blueprint.route("/")
 def blog_index():
     return render_template("blog.html")
 
-@blog_blueprint.route("/blog_list/<int:post_id>")
-def post(post_id):
-    post=Post.query.get_or_404(post_id)
+"""
+主要展示所有文章，只按发表时间顺序
+"""
+@blog_blueprint.route("/post_list")
+def post_list():
+    post_list = Post.query.order_by(Post.publish_date.desc()).all()
     return render_template("blog/post.html",post=post)
-@blog_blueprint.route("/blog_list")
-@blog_blueprint.route("/blog_list/<string:control>/<int:post_id>")
+
+"""
+显示单个文章
+"""
+@blog_blueprint.route("/post_list/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("blog/post.html", post=post)
+
+"""
+显示单个文章的用户操作
+"""
+@blog_blueprint.route("/post_list/<int:post_id>/<string:control>")
+def post_id_control():
+    pass
